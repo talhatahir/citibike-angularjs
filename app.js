a46305339bb0466ec42bc690607d1ba16d6ddd01
talhatahir <trinitro619@gmail.com>
2018-03-28 22:11:37 +0500

    Added chart with search

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
  // create the module and name it citibikeApp
         // also include ngRoute for all our routing needs
-        var citibikeApp = angular.module('citibikeApp', ['ngRoute']);
+        var citibikeApp = angular.module('citibikeApp', ['ngRoute','zingchart-angularjs']);
 
         // configure our routes
         citibikeApp.config(function($routeProvider) {
-            $routeProvider
-                // route for the search page
-                .when('/search', {
-                    templateUrl : 'pages/search.html',
-                    controller  : 'searchController'
-                })
-                .otherwise({redirectTo : '/'});
+           
                
         });
+           
     
-       
     
-        citibikeApp.controller('searchController', function($scope) {
-            $scope.message = 'Look! I am an searchController page.';
-        });
     
        
     
--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -6,6 +6,64 @@ var citibikeApp = angular.module('citibikeApp');
     var resArr=[];    
     $scope.markers = [];
     $scope.geonames = [];
+    //setup chart        
+    $scope.myJson = {
+        globals: {
+            shadow: false,
+            fontFamily: "Verdana",
+            fontWeight: "100"
+        },
+        type: "pie",
+        backgroundColor: "#fff",
+
+        legend: {
+            layout: "x3",
+            position: "50%",
+            borderColor: "transparent",
+            marker: {
+                borderRadius: 7,
+                borderColor: "transparent"
+            }
+        },
+        tooltip: {
+            text: "count = %v"
+        },
+        plot: {
+            refAngle: "-90",
+            borderWidth: "0px",
+            valueBox: {
+                placement: "in",
+                text: "%npv %",
+                fontSize: "15px",
+                textAlpha: 1,
+            }
+        },
+        series: [
+              {
+                values: [0],
+                text: "Bikes Available"
+          
+              }, {
+                values: [0],
+                text: "Ebikes Available"
+          
+              }, {
+                values: [0],
+                text: "Bikes Disabled"
+          
+              }, {
+                values: [6],
+                text: "Docks Available"
+          
+              }, {
+                values: [0],
+                text: "Docks Disabled"
+          
+              }
+
+        ]
+    };
+
         
     //init Maps for use
     var mapOptions = {
@@ -16,6 +74,7 @@ var citibikeApp = angular.module('citibikeApp');
 
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);    
 
+    
     //prepare call for fetchin data from both jsons
     var myDataPromise1 = stationInfoService.getData("https://gbfs.citibikenyc.com/gbfs/en/station_information.json");
     var myDataPromise2 = stationInfoService.getData("https://gbfs.citibikenyc.com/gbfs/en/station_status.json");
@@ -26,8 +85,9 @@ var citibikeApp = angular.module('citibikeApp');
         secondResponse: myDataPromise2
       });
 
+    //Get data from Promises
+    combinedData.then(function(response) {  
 
-    combinedData.then(function(response) {     
     var stInf=[];
     stInf=response.firstResponse;
 
@@ -46,75 +106,69 @@ var citibikeApp = angular.module('citibikeApp');
         if(calcPcntage < 0) calcPcntage=0;
         pctAge.push(calcPcntage); 
         
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(stInf[i].lat, stInf[i].lon),
+            title: stInf[i].name,
+            icon: (calcPcntage == 0) ? mapIcons[1] :(calcPcntage < 50) ? mapIcons[0] : mapIcons[2] ,
+            num_bikes_available:stStat[i].num_bikes_available,
+            num_ebikes_available:stStat[i].num_ebikes_available,
+            num_bikes_disabled:stStat[i].num_bikes_disabled,
+            num_docks_available:stStat[i].num_docks_available,
+            num_docks_disabled:stStat[i].num_docks_available
+        });
 
-        if(calcPcntage == 0){
-            var marker = new google.maps.Marker({
-                map: $scope.map,
-                position: new google.maps.LatLng(stInf[i].lat, stInf[i].lon),
-                title: stInf[i].name,
-                icon:mapIcons[1],
-                pcnt: calcPcntage,
-                searchTag:stInf[i].name
-            });    
 
-        }else if(calcPcntage < 50){
-
-            var marker = new google.maps.Marker({
-                map: $scope.map,
-                position: new google.maps.LatLng(stInf[i].lat, stInf[i].lon),
-                title: stInf[i].name,
-                icon:mapIcons[0],
-                pcnt: calcPcntage,
-                searchTag:stInf[i].name
-            });
-
-        }else{
-            var marker = new google.maps.Marker({
-                map: $scope.map,
-                position: new google.maps.LatLng(stInf[i].lat, stInf[i].lon),
-                title: stInf[i].name,
-                icon:mapIcons[2],
-                pcnt: calcPcntage,
-                searchTag:stInf[i].name
-            });
-
-        }        
-        
         //pushing google map markers in scope marker array
         $scope.markers.push(marker);
-
         //pushing station geo names in scope array
         $scope.geonames.push(stInf[i].name);
+        
+       
      
     }
 
-    Highcharts.chart('container', {
-        title: {
-          text: 'Percentage of bikes available at each station'
-        },
-  
-        xAxis: {
-          categories: $scope.geonames,
-          title: {
-            text: 'Station Name'
-            }
-        },
-
-        yAxis: {
-            title: {
-                text: '%age of Bikes being used'
-            }
-        },
-  
-        series: [{
-          name: '% of Bike in use',
-          data: pctAge
-        }]
-      });
-
+    $scope.initChart();
        
     }); 
+
+    //initChart
+    $scope.initChart = function(){
+
+        var totalBikesAvailable=0;
+        var totalEbikesAvailable=0;
+        var totalBikesDisabled=0;
+        var totalDocksAvailable=0;
+        var totalDocksDisabled=0;
+
+        for(var i=0; i<$scope.markers.length;i++){
+            totalBikesAvailable+=$scope.markers[i].num_bikes_available;
+            totalEbikesAvailable+=$scope.markers[i].num_ebikes_available;
+            totalBikesDisabled+=$scope.markers[i].num_bikes_disabled;
+            totalDocksAvailable+=$scope.markers[i].num_docks_available;
+            totalDocksDisabled+=$scope.markers[i].num_docks_disabled;
+        }
+
+        $scope.myJson.series[0].values.pop();
+        $scope.myJson.series[0].values.push(totalBikesAvailable);
+
+        $scope.myJson.series[1].values.pop();
+        $scope.myJson.series[1].values.push(totalEbikesAvailable);
+
+        $scope.myJson.series[2].values.pop();
+        $scope.myJson.series[2].values.push(totalBikesDisabled);
+
+        $scope.myJson.series[3].values.pop();
+        $scope.myJson.series[3].values.push(totalDocksAvailable);
+
+        $scope.myJson.series[4].values.pop();
+        $scope.myJson.series[4].values.push(totalDocksDisabled);
+       
+
+    }
+
     
+    //searchMarker on Google Map with Radius and Station Name
     $scope.searchMarker = function(){
 
         var foundQuery;
@@ -124,12 +178,8 @@ var citibikeApp = angular.module('citibikeApp');
         } 
 
 
-        for(var i=0;i<$scope.markers.length;i++){
-            if($scope.markers[i].title==$scope.searchVal){
-                foundQuery=$scope.markers[i];
-                break;
-            }
-        }
+        foundQuery=$scope.searchQueryMarker($scope.searchVal);
+
 
         for(var i=0;i<$scope.markers.length;i++){
             $scope.markers[i].setMap(null);
@@ -151,11 +201,55 @@ var citibikeApp = angular.module('citibikeApp');
             
             if(dist<=radiusInm){
                 $scope.markers[i].setMap($scope.map);
-                console.log(dist);
+                
             }      
                 
         }
         
     }
+    //Function to query a value in $scope.markers
+    $scope.searchQueryMarker=function(searchKey){
+
+        for(var i=0;i<$scope.markers.length;i++){
+            if($scope.markers[i].title== searchKey){                 
+                return $scope.markers[i];
+            }
+        }
+        
+        return 0;
+
+    }
+    //Show station specifc chart data
+    $scope.showChartData=function(){
+
+        var foundQuery;
+
+        if($scope.searchStation=='' || $scope.searchStation==undefined){
+            $scope.initChart();
+            return;
+        }
+
+        foundQuery=$scope.searchQueryMarker($scope.searchStation);  
+
+        $scope.myJson.series[0].values.pop();
+        $scope.myJson.series[0].values.push(foundQuery.num_bikes_available);
+
+        $scope.myJson.series[1].values.pop();
+        $scope.myJson.series[1].values.push(foundQuery.num_ebikes_available);
+
+        $scope.myJson.series[2].values.pop();
+        $scope.myJson.series[2].values.push(foundQuery.num_bikes_disabled);
+
+        $scope.myJson.series[3].values.pop();
+        $scope.myJson.series[3].values.push(foundQuery.num_docks_available);
+
+        $scope.myJson.series[4].values.pop();
+        $scope.myJson.series[4].values.push(foundQuery.num_docks_disabled);
+
+
+    }
+
+    
+
 
 }]);
